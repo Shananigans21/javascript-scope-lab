9a7253f254058dcc407e91fa4f6b0bd097240089
Shana Lynch <shana.lynch21@gmail.com>
2025-04-16 18:25:27 -0400

    javascript-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+console.log ("Welcome to Flatburger");
+
+// Step 1: Global Scope
+let burgers =["Hamburger", "Cheeseburger"];
+let featuredDrinks = ['Strawberry Milkshake']
+  
+// Step 2: Function and Function scope
+ function addBurger () {
+ let newBurger = "Flatburger"; //function-scoped
+    burgers.push(newBurger);
+ }
+
+// Step 3: Block scope
+if(true) {
+    let anothernewBurger = "Maple Bacon Burger";
+    burgers.push(anothernewBurger);
+
+    // Step 4: Function to change the featured drinks
+    function changeFeatureDrinks () {
+        featuredDrinks = ("The JavaShake");
+    }
+}
+
+console.log(burgers);
+addBurger();
+console.log(burgers);
+console.log(featuredDrinks);
+changeFeatureDrinks();
+console.log(featuredDrinks);
